5b1c89a947e8abfe89d9a1793e5b803a9319649d
millercc23 <91136427+millercc23@users.noreply.github.com>
2022-01-24 22:26:27 -0500

    setting up falt for if the client disc

--- applications/consumer/main.py
+++ applications/consumer/main.py
@@ -5,7 +5,9 @@ def main():
     'frontendSend',
     consumer.callback,
     auto_ack=True
-)
+    )
+    if(mqtt.client1.on_unsubscribe()):
+        mqtt.on_disconnect()
     pass
 if __name__ =="__main__":
     main()
--- applications/consumer/mqtt.py
+++ applications/consumer/mqtt.py
@@ -1,11 +1,24 @@
+import logging
 import paho.mqtt.client as paho
-
+import time
 def on_publish(client, userdata, result):
     print('data published \n')
     pass
 
+def on_log(client, userdata, level, buf):
+    print('log: {0}', buf)
+
+def on_disconnect(client, userdata, rc=0):
+    logging.debug("Disconnected result code: {0}", str(rc))
+    client1.loop_stop()
 
 broker = '199.244.104.202'
 client1= paho.Client('control1')
 client1.on_publish = on_publish
-client1.connect(broker)
+client1.on_log = on_log
+client1.connect(broker,'',60)#keeps the mqtt broker connection open for 60 seconds
+client1.loop_start()
+time.sleep(4)
+
+if(client1.on_unsubscribe()):
+    on_disconnect()
