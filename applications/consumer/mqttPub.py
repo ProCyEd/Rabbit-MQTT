2ddec0c45534cd390a2bfd0034a0aa5ae9585701
millercc23 <91136427+millercc23@users.noreply.github.com>
2022-01-30 21:23:18 -0500

    2

--- applications/consumer/mqttPub.py
+++ applications/consumer/mqttPub.py
@@ -1,13 +1,18 @@
 import logging
 import paho.mqtt.client as paho
 import time
-
+import json
 
 client1= paho.Client('control1')
 def on_disconnect(client, userdata, rc=0):
     logging.debug("Disconnected result code: {0}", str(rc))
     client1.loop_stop()
-    
+def convert_JSON(message):
+    conv = json.loads(message)
+    # boxId = conv['boxId']
+    # equipmentId = conv['equipmentId']
+    msg = conv['message']
+    ## this is where we run the queries to get the boxname and equipment ip
 
 def connect(body):
     def on_publish(client, userdata, result):
@@ -26,7 +31,7 @@ def connect(body):
     client1.on_publish = on_publish
     client1.on_log = on_log
     client1.connect(broker,1883,60)#keeps the mqtt broker connection open for 60 seconds
-    client1.loop_start()
+    client1.loop_forever()
     ret=client1.publish("test", body)
     time.sleep(4)
 
