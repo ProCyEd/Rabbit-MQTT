de60cb9359df408b678564a3b20896c1ddd24f7a
millercc23 <cam.miller521@gmail.com>
2022-03-01 19:46:40 -0500

    changes

--- applications/SQLite/StoreProcedures/insertEquipment.py
+++ applications/SQLite/StoreProcedures/insertEquipment.py
@@ -6,7 +6,7 @@ def insertEquipment(equipment_id, box_id, equipment_name, equipment_state, equip
 
     c = conn.cursor()
     # Query using prepared statement
-    query = """INSERT INTO boxes (equipment_id, box_id, equipment_name, equipment_state, equipment_ip)) 
+    query = """INSERT INTO equipment (equipment_id, box_id, equipment_name, equipment_state, equipment_ip)) 
                 VALUES (%s, %s, %s,%s,%s)"""
     tuple = (equipment_id, box_id, equipment_name, equipment_state, equipment_ip)
     c.execute(query, tuple)
